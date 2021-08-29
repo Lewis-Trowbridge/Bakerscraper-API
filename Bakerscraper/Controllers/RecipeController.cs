9b87c8716a18d612e79d6190b28fe4bfe72cf6d2
Lewis Trowbridge <lewbob9927@gmail.com>
2021-08-29 16:44:42 +0100

    Added Swagger documenation annotations

--- Bakerscraper/Controllers/RecipeController.cs
+++ Bakerscraper/Controllers/RecipeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +9,14 @@ using Bakerscraper.Models;
 
 namespace Bakerscraper.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class RecipeController : ControllerBase
     {
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpGet("search")]
         public Recipe Search(string searchString)
         {
             throw new NotImplementedException();
