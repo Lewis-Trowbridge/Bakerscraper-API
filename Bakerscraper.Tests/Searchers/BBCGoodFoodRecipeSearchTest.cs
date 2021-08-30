cd3d6bf5e51cfe83f14b44cd9e3d7782400141fe
Lewis Trowbridge <lewbob9927@gmail.com>
2021-08-30 13:58:10 +0100

    Retrieve recipe name

--- Bakerscraper.Tests/Searchers/BBCGoodFoodRecipeSearchTest.cs
+++ Bakerscraper.Tests/Searchers/BBCGoodFoodRecipeSearchTest.cs
@@ -41,20 +41,47 @@ namespace Bakerscraper.Tests.Searchers
         [Fact]
         public async void BBCGoodFoodSearcher_GivenCorrectString_ReturnsCorrectOutput()
         {
-           
-            var testSearchString = "test";
-            var testHtml = File.ReadAllText("Searchers/Assets/testsearch.html");
-            var testResponse = new HttpResponseMessage { Content = new StringContent(testHtml) };
+            // Load test data
+            var testSearchRequest = new HttpRequestMessage(HttpMethod.Get, "https://www.bbcgoodfood.com/search/recipes?q=test");
+            var testMicrowaveRequest = new HttpRequestMessage(HttpMethod.Get, "https://www.bbcgoodfood.com//recipes/microwave-mug-cake");
+            var testMeltRequest = new HttpRequestMessage(HttpMethod.Get, "https://www.bbcgoodfood.com//recipes/melt-middle-mug-cake");
+            var testsearchHtml = File.ReadAllText("Searchers/Assets/testsearch.html");
+            var testMicrowaveHtml = File.ReadAllText("Searchers/Assets/testmicrowave.html");
+            var testMeltHtml = File.ReadAllText("Searchers/Assets/testmeltinthemiddle.html");
+            var testSearchResponse = new HttpResponseMessage { Content = new StringContent(testsearchHtml) };
+            var testMicrowaveResponse = new HttpResponseMessage { Content = new StringContent(testMicrowaveHtml) };
+            var testMeltResponse = new HttpResponseMessage { Content = new StringContent(testMeltHtml) };
+
+            // Set up mock HTTP responses
             var mockHandler = new Mock<HttpMessageHandler>();
+
             mockHandler
-                .Protected()
+               .Protected()
                .Setup<Task<HttpResponseMessage>>(
                   "SendAsync",
-                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.Is<HttpRequestMessage>(request => request.RequestUri == testSearchRequest.RequestUri),
                   ItExpr.IsAny<CancellationToken>())
-               .ReturnsAsync(testResponse);
+               .ReturnsAsync(testSearchResponse);
+
+            mockHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(request => request.RequestUri == testMicrowaveRequest.RequestUri),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(testMicrowaveResponse);
+
+            mockHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.Is<HttpRequestMessage>(request => request.RequestUri == testMeltRequest.RequestUri),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(testMeltResponse);
+
             var mockClient = new HttpClient(mockHandler.Object);
             var testSearcher = new BBCGoodFoodRecipeSearch(mockClient);
+            var testSearchString = "test";
 
             var expectedRecipes = BBCGoodFoodRecipeSearchTestHelper.GetBBCGoodFoodRecipes();
 
--- Bakerscraper/Searchers/BBCGoodFoodRecipeSearch.cs
+++ Bakerscraper/Searchers/BBCGoodFoodRecipeSearch.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.IO;
 using System.Net.Http;
+using HtmlAgilityPack;
+using VDS.RDF;
+using VDS.RDF.Parsing;
 
 namespace Bakerscraper.Searchers
 {
@@ -13,6 +17,9 @@ namespace Bakerscraper.Searchers
 
         private HttpClient httpClient;
         private const string baseUrl = "https://www.bbcgoodfood.com/";
+        private readonly Uri schemaNameUri = new("http://schema.org/name");
+        private readonly Uri schemaRecipeIngredientUri = new("http://schema.org/recipeIngredient");
+        private readonly Uri schemaRecipeInstructionsUri = new("http://schema.org/recipeInstructions");
 
         public BBCGoodFoodRecipeSearch()
         {
@@ -26,14 +33,67 @@ namespace Bakerscraper.Searchers
 
         public async Task<List<Recipe>> Search(string searchString)
         {
-            var searchHTML = await GetGoodFoodHTML(searchString);
-            return new List<Recipe>();
+            var recipes = new List<Recipe>();
+            var searchHTML = await GetGoodFoodSearchHTML(searchString);
+            var recipeUris = GetGoodFoodRecipeUris(searchHTML);
+            if (recipeUris.Count() == 0)
+            {
+                return recipes;
+            }
+            var recipeTasks = new List<Task<Recipe>>();
+            foreach (var recipeUri in recipeUris)
+            {
+                recipeTasks.Add(GetRecipeFromLink(recipeUri));
+            }
+            Task.WaitAll(recipeTasks.ToArray());
+            foreach (var recipeTask in recipeTasks)
+            {
+                recipes.Add(recipeTask.Result);
+            }
+            return recipes;
         }
 
-        private async Task<string> GetGoodFoodHTML(string searchString)
+        private async Task<string> GetGoodFoodSearchHTML(string searchString)
         {
             var response = await httpClient.GetAsync(baseUrl + "search/recipes?q=" + HttpUtility.UrlEncode(searchString));
             return await response.Content.ReadAsStringAsync();
+        }
+
+        private IEnumerable<Uri> GetGoodFoodRecipeUris(string searchHTML)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(searchHTML);
+
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//a[@class='img-container img-container--square-thumbnail']");
+            if (nodes != null)
+            {
+                return nodes.Select(node => new Uri(baseUrl + node.Attributes["href"].Value));
+            }
+            else
+            {
+                return new List<Uri>();
+            }
+        }
+
+        private async Task<Recipe> GetRecipeFromLink(Uri recipeUri)
+        {
+            var recipe = new Recipe();
+
+            var response = await httpClient.GetAsync(recipeUri);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(await response.Content.ReadAsStringAsync());
+
+            var jsonLdString = doc.DocumentNode.SelectSingleNode("//script[@type='application/ld+json']").InnerText;
+
+            var jsonLdProcessor = new JsonLdParser();
+            var recipeTripleStore = new ThreadSafeTripleStore();
+
+            jsonLdProcessor.Load(recipeTripleStore, new StringReader(jsonLdString));
+
+            var nameNode = (LiteralNode)recipeTripleStore.GetTriplesWithPredicate(schemaNameUri).First().Object;
+            recipe.Name = nameNode.Value;
+
+            return recipe;
         }
 
     }
