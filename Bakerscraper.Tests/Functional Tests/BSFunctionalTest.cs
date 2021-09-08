60aa83c386f538cfc107d8c2712d5eb38b11bb8f
Lewis Trowbridge <lewbob9927@gmail.com>
2021-09-08 20:04:49 +0100

    Added Swagger functional tests

---
+++ Bakerscraper.Tests/Functional Tests/BSFunctionalTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Bakerscraper.Tests.Functional_Tests
+{
+    public class BSFunctionalTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public BSFunctionalTest()
+        {
+            _factory = new WebApplicationFactory<Startup>();
+        }
+
+        [Fact]
+        public async void Get_SwaggerPage_ReturnsSuccessCode()
+        {
+            var expectedResposeCode = HttpStatusCode.OK;
+
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/swagger");
+
+            Assert.Equal(expectedResposeCode, response.StatusCode);
+
+        }
+        
+        [Fact]
+        public async void Get_SwaggerPage_ReturnsHTML()
+        {
+            var expectedResponseType = "text/html";
+
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/swagger");
+
+            Assert.Equal(expectedResponseType, response.Content.Headers.ContentType.MediaType);
+        }
+
+    }
+}
